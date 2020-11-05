fa87fae500290e6e3b555fd4349327db1bb0d98c
Joey Hoek <j.hoek@clappform.com>
2020-11-05 16:54:31 +0100

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="Clappform",
-    version="1.2.0",
+    version="1.2.1",
     description="Clappform API Connector",
     long_description=readme(),
     long_description_content_type="text/markdown",
