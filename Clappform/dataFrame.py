9b4261304c82f02070be4eba07d1ed6691e1d009
Diego Tolen <73528761+DiegoTolenClappform@users.noreply.github.com>
2022-02-16 12:44:26 +0100

    Update dataFrame.py

--- Clappform/dataFrame.py
+++ Clappform/dataFrame.py
@@ -22,7 +22,7 @@ class _DataFrame:
         self.app_id = app
         self.collection_id = collection
 
-    def Read(self, original=True, itemsPerRun=500, n_jobs = 1):
+    def Read(self, original=True, itemsPerRun=100, n_jobs = 1):
 
         self.data = []
         if not Auth.tokenValid():
