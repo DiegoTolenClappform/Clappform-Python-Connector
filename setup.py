fe33ed5c257296f215991435efc1169476acdc0b
bharkema <79573684+bharkema@users.noreply.github.com>
2022-04-04 13:24:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="Clappform",
-    version="1.7.7",
+    version="1.7.72",
     description="Clappform API Connector",
     long_description=readme(),
     long_description_content_type="text/markdown",
