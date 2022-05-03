13436f3b6bc6f8daf3e346914579930fc23fcefe
PandysActor <y.jhingur@clappform.com>
2022-05-03 15:32:54 +0200

    .

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="Clappform",
-    version="1.8.36",
+    version="1.8.37",
     description="Clappform API Connector",
     long_description=readme(),
     long_description_content_type="text/markdown",
