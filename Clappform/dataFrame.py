f6ea00faf5b617b8f285dbfc21be6ca3c22d2b46
PandysActor <y.jhingur@clappform.com>
2022-05-10 15:44:24 +0200

    .

--- Clappform/dataFrame.py
+++ Clappform/dataFrame.py
@@ -51,8 +51,8 @@ class _DataFrame:
                     res_data.append(item["data"])
 
                 # Sleep for elapsed time to not get marked as DDOS
-                print(i, response.elapsed)
-                time.sleep(response.elapsed)
+                print(i, response.elapsed.total_seconds())
+                time.sleep(response.elapsed.total_seconds())
                 yield pd.DataFrame(res_data)
             except requests.exceptions.RequestException as exception:
                 resp = exception.response
