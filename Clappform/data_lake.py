03ac8e9858fb5f0bffc0e2bd5916c315806257fd
Fabio van Haarlem <f.vanhaarlem@clappform.com>
2022-06-22 15:41:49 +0200

    Added data lake router calls and tests

---
+++ Clappform/data_lake.py
@@ -0,0 +1,72 @@
+from .settings import settings
+from .dataFrame import _DataFrame
+from .item import _Item
+from .auth import Auth
+import requests
+import math
+import pandas as pd
+
+
+class _DataLake:
+    app_id = None
+    collection_id = None
+
+    def __init__(self, app=None, collection=None):
+        self.app_id = app
+        self.id = collection
+
+    def GetAllCollections(self):
+        if not Auth.tokenValid():
+            Auth.refreshToken()
+
+        response = requests.delete(f'{settings.baseURL}api/metric/get_all_collections/datalake',
+                                   headers={'Authorization': 'Bearer ' + settings.token})
+
+        if response.json()["code"] is 200:
+            if "data" in response.json():
+                return response.json()["data"]
+            else:
+                return []
+        else:
+            raise Exception(response.json()["message"])
+
+    def GetAddPermissions(self):
+        if not Auth.tokenValid():
+            Auth.refreshToken()
+
+        response = requests.delete(f'{settings.baseURL}api/metric/add_permissions/datalake',
+                                   headers={'Authorization': 'Bearer ' + settings.token})
+
+        if response.json()["code"] is 200:
+            if "data" in response.json():
+                return response.json()["data"]
+            else:
+                return []
+        else:
+            raise Exception(response.json()["message"])
+
+    def DeleteApp(self):
+        if not Auth.tokenValid():
+            Auth.refreshToken()
+
+        response = requests.delete(settings.baseURL + f'api/metric/delete_app/{self.app_id}',
+                                   headers={'Authorization': 'Bearer ' + settings.token})
+
+        if response.json()["code"] is 200:
+            return True
+        else:
+            raise Exception(response.json()["message"])
+
+    def DeleteCollection(self):
+        if not Auth.tokenValid():
+            Auth.refreshToken()
+
+        response = requests.delete(settings.baseURL + f'api/metric/delete_collection/{self.app_id}/{self.collection_id}',
+                                   headers={'Authorization': 'Bearer ' + settings.token})
+
+        if response.json()["code"] is 200:
+            if "data" in response.json():
+                raise Exception(response.json())
+            return True
+        else:
+            raise Exception(response.json()["message"])
---
+++ test/test_data_lake.py
@@ -0,0 +1,78 @@
+# Standard lib modules
+import unittest
+from unittest.mock import patch
+import pandas as pd
+
+from .context import Clappform
+from .settings import settings
+from Clappform.collection import _Collection
+from Clappform.app import App
+
+
+class TestDataFrame(unittest.TestCase):
+    global_dataframe_data = ""
+
+    def setUp(self):
+        # Set up all needed vars
+        print("=====[ Setting up vars for dataframe testing ]=====")
+        self.url = settings.baseURL
+        self.username = settings.username
+        self.password = settings.password
+        self.app_id = "test_app"
+        self.collection_id = "test_collection"
+        self.dataframe_data = ""
+        self.dataframe_query = ""
+
+        # Vars for setting up an app and collection.
+        self.app_id = "test_app"
+        self.app_name = "Test App"
+        self.app_desc = "This app gives ... insights on ... subject."
+        self.app_icon = "home-icon"
+
+        self.collection_id = "test_collection"
+        self.collection_name = "Test Collection"
+        self.collection_desc = "This app gives ... insights on ... subject."
+        self.collection_encryption = True
+        self.collection_logging = True
+        self.collection_sources = []
+        Clappform.Auth(baseURL=self.url, username=self.username, password=self.password)
+    ##### NOTES #####
+    # I am adding letters to functions so the system will execute the tests in a certain order.
+
+    def test_getAllCollections(self):
+        print("=====[ Reading collection and creating dataframe ]=====")
+        for result in App("clappform_logs").Collection("api_logs").DataFrame().Read(original=True, n_jobs=1):
+            if result is not None:
+                TestDataFrame.global_dataframe_data = result
+        print(TestDataFrame.global_dataframe_data)
+        assert TestDataFrame.global_dataframe_data is not None
+        print("=====[ Done reading collection and creating dataframe ]=====")
+
+    def test_getAddPermissions(self):
+        print("=====[ Syncing collection and dataframe ]=====")
+        App("default").Collection().Create(slug=self.collection_id, name=self.collection_desc, description=self.collection_desc, encryption=self.collection_encryption, logging=self.collection_logging, sources=self.collection_sources)
+        resp = App("default").Collection(self.collection_id).DataFrame().Synchronize(dataframe=TestDataFrame.global_dataframe_data, n_jobs=0)
+        assert resp is True
+        print("=====[ Done Syncing collection and dataframe ]=====")
+
+    def test_deleteApp(self):
+        print("=====[ Appending collection and dataframe ]=====")
+        # We first need to create a app before we can delete one
+        App("default").Collection(self.collection_id).DataFrame().Append(dataframe=TestDataFrame.global_dataframe_data, n_jobs=1, show=False)
+
+        resp = App("default").Collection(self.collection_id).DataFrame().Append(dataframe=TestDataFrame.global_dataframe_data, n_jobs=1, show=False)
+        assert resp is True
+        print("=====[ Done appending collection and dataframe ]=====")
+
+    def test_deleteCollection(self):
+        print("=====[ Querying collection and dataframe ]=====")
+        # We first need to create a app and collection before we can delete one
+        App("default").Collection(self.collection_id).DataFrame().Append(dataframe=TestDataFrame.global_dataframe_data, n_jobs=1, show=False)
+
+        self.dataframe_query = App("default").Collection(self.collection_id).DataFrame().Query(filters={}, projection={}, sorting={}, original=True)
+        print(self.dataframe_data)
+
+        # Delete the app we created
+        App("default").Collection(self.collection_id).DataFrame().Append(dataframe=TestDataFrame.global_dataframe_data, n_jobs=1, show=False)
+        assert self.dataframe_data is not None
+        print("=====[ Done querying collection and dataframe ]=====")
