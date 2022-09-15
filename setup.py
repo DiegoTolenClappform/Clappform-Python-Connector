acccad1fcb7d60bd5827fbcfffc51e198a6e4cdf
Wybren Terpstra <wterpstra@protonmail.com>
2022-09-15 15:11:57 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
-import _version
+import Clappform
+
+
+url = "https://github.com/DiegoTolenClappform/Clappform-Python-Connector"
+
 
 def readme():
-    with open("README.md") as f:
-        return f.read()
+    with open("README.md") as fd:
+        return fd.read()
+
 
 setup(
     name="Clappform",
-    version=_version.__version__,
+    version=Clappform.__version__,
     description="Clappform API Connector",
     long_description=readme(),
     long_description_content_type="text/markdown",
@@ -17,17 +22,17 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    url="https://github.com/DiegoTolenClappform/Clappform-Python-Connector",
-    download_url = 'https://github.com/DiegoTolenClappform/Clappform-Python-Connector/archive/' + _version.__version__ + '.tar.gz',
-    author="Clappform",
-    author_email="info@clappform.com",
+    url=url,
+    download_url="{}/archive{}{}".format(
+        url,
+        Clappform.__version__,
+        ".tar.gz",
+    ),
+    author=Clappform.__author__,
+    author_email=Clappform.__email__,
     keywords="api connector",
-    license="MIT",
-    packages = ['Clappform'],
-    install_requires=[
-        "pandas",
-        "PyGithub",
-        "pyarrow"
-    ],
+    license=Clappform.__license__,
+    packages=["Clappform"],
+    install_requires=["PyGithub", "numpy", "pandas", "pyarrow", "redis", "requests"],
     include_package_data=True,
 )
